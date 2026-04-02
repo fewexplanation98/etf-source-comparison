ef738b23915786f075d5488bceba34dac68790e9
fewexplanation98 <alessandrosilvestri98@gmail.com>
2026-04-02 23:03:40 +0200

    Update performance.js

--- dashboard/api/performance.js
+++ dashboard/api/performance.js
@@ -3,11 +3,18 @@ import path from "path";
 
 export default async function handler(req, res) {
   try {
-    const filePath = path.join(process.cwd(), "data", "justetf-results.json");
+    const filePath = path.join(
+      process.cwd(),
+      "public",
+      "data",
+      "justetf-results.json"
+    );
+
     const raw = fs.readFileSync(filePath, "utf8");
     const json = JSON.parse(raw);
 
     res.setHeader("Cache-Control", "s-maxage=1800, stale-while-revalidate=3600");
+
     return res.status(200).json(json);
   } catch (error) {
     return res.status(500).json({
